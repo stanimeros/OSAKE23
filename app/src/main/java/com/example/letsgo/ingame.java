5b3a38a4622b6227289466400f27f1790c0f7779
stanimeros <pantelisstanimeros@outlook.com>
2022-06-16 13:32:50 +0300

    v2

--- app/src/main/java/com/example/letsgo/ingame.java
+++ app/src/main/java/com/example/letsgo/ingame.java
@@ -23,6 +23,8 @@ public class ingame extends AppCompatActivity {
     CountDownTimer timer;
     TextView periodboard;
     TextView timeboard;
+    boolean running = false;
+    boolean finished = false;
     Match m;
     int id;
 
@@ -42,83 +44,61 @@ public class ingame extends AppCompatActivity {
         t1.setText(m.getAway());
 
         TextView score = findViewById(R.id.scoreboard);
-        String sc = MySQL.getMatchColumn(m.getId(),"homePoints")+"  -  " + MySQL.getMatchColumn(m.getId(),"awayPoints");
+        String sc = MySQL.getMatchColumn(m.getId(), "homePoints") + "  -  " + MySQL.getMatchColumn(m.getId(), "awayPoints");
         score.setText(sc);
 
         TextView periodboard = findViewById(R.id.periodboard);
-        periodboard.setText(MySQL.getMatchColumn(m.getId(),"period")+"η Περίοδος");
+        periodboard.setText(MySQL.getMatchColumn(m.getId(), "period") + "η Περίοδος");
 
         timeboard = findViewById(R.id.timeboard);
+        startTimerWorking();
 
-        int timestamp = MySQL.getMatchColumn(m.getId(),"timestamp");
-        if (timestamp<0) {
-            secs=599000;
-            updateText();
-        }
-        else {
-            timestamp = getCurrentSeconds() - timestamp;
-            timestamp = MySQL.START_TIME - timestamp*1000;
-            secs=timestamp;
+            new LogoDownload((ImageView) findViewById(R.id.team1logo))
+                    .execute(MySQL.getTeamLogo(m.getHome()));
 
-            if (MySQL.getMatchColumn(m.getId(),"break")==0)
-            {
-                startTimer();
-            }else
-            {
-                int br = MySQL.getMatchColumn(m.getId(),"break");
-                timestamp = MySQL.getMatchColumn(m.getId(),"timestamp");
-                timestamp = br - timestamp;
-                secs = MySQL.START_TIME - timestamp*1000;
-                updateText();
-            }
-        }
+            new LogoDownload((ImageView) findViewById(R.id.team2logo))
+                    .execute(MySQL.getTeamLogo(m.getAway()));
 
-        new LogoDownload((ImageView) findViewById(R.id.team1logo))
-                .execute(MySQL.getTeamLogo(m.getHome()));
-
-        new LogoDownload((ImageView) findViewById(R.id.team2logo))
-                .execute(MySQL.getTeamLogo(m.getAway()));
-
-        ImageView back = findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gotomatches();
-            }
-        });
-
-        BottomNavigationView bottomNavigationView2;
-        live_flow live_flow = new live_flow();
-        live_team_stats live_team_stats = new live_team_stats();
-        live_personal_stats live_personal_stats = new live_personal_stats();
-
-        live_flow.setArguments(bundle);
-        live_team_stats.setArguments(bundle);
-        live_personal_stats.setArguments(bundle);
-
-        bottomNavigationView2 = findViewById(R.id.bottomNavigationView2);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2,live_flow).commit();
-
-        bottomNavigationView2.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.live_flow:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2,live_flow).commit();
-                        return true;
-
-                    case R.id.live_team_stats:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2,live_team_stats).commit();
-                        return true;
-
-                    case R.id.live_personal_stats:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2,live_personal_stats).commit();
-                        return true;
+            ImageView back = findViewById(R.id.back);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    gotomatches();
                 }
-                return false;
-            }
-        });
+            });
+
+            BottomNavigationView bottomNavigationView2;
+            live_flow live_flow = new live_flow();
+            live_team_stats live_team_stats = new live_team_stats();
+            live_personal_stats live_personal_stats = new live_personal_stats();
+
+            live_flow.setArguments(bundle);
+            live_team_stats.setArguments(bundle);
+            live_personal_stats.setArguments(bundle);
+
+            bottomNavigationView2 = findViewById(R.id.bottomNavigationView2);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, live_flow).commit();
+
+            bottomNavigationView2.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.live_flow:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, live_flow).commit();
+                            return true;
+
+                        case R.id.live_team_stats:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, live_team_stats).commit();
+                            return true;
+
+                        case R.id.live_personal_stats:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, live_personal_stats).commit();
+                            return true;
+                    }
+                    return false;
+                }
+            });
     }
 
     private void gotomatches(){
@@ -126,16 +106,43 @@ public class ingame extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void startTimerWorking()
+    {
+        int timestamp = MySQL.getMatchColumn(m.getId(), "timestamp");
+        if (timestamp < 0) {
+            secs = 599000;
+            updateText();
+        } else {
+            timestamp = getCurrentSeconds() - timestamp;
+            timestamp = MySQL.START_TIME - timestamp * 1000;
+            secs = timestamp;
+
+            if (MySQL.getMatchColumn(m.getId(), "break") == 0) {
+                running = true;
+                startTimer();
+            } else {
+                running = false;
+                int br = MySQL.getMatchColumn(m.getId(), "break");
+                timestamp = MySQL.getMatchColumn(m.getId(), "timestamp");
+                timestamp = br - timestamp;
+                secs = MySQL.START_TIME - timestamp * 1000;
+                updateText();
+            }
+        }
+    }
+
     private void startTimer(){
         timer = new CountDownTimer(secs, 1000) {
             @Override
             public void onTick(long l) {
+                running = true;
                 secs = l;
                 updateText();
             }
 
             @Override
             public void onFinish() {
+                finished = true;
                 if (MySQL.getMatchColumn(m.getId(),"period")==4)
                 {
                     try {
@@ -162,6 +169,35 @@ public class ingame extends AppCompatActivity {
         timeboard.setText(timeFormatted);
     }
 
+    private void updateTimer(){
+        if (MySQL.getMatchColumn(m.getId(), "break") != 0) {
+            try {
+                timer.cancel();
+                running = false;
+            }catch (Exception e){}
+        }else
+        {
+            if (!running)
+            {
+                startTimerWorking();
+            }else if (finished)
+            {
+                startTimerWorking();
+            }
+        }
+    }
+
+    private void updatePeriod(){
+        periodboard = findViewById(R.id.periodboard);
+        if (MySQL.getMatchColumn(m.getId(),"isFinished")==1)
+        {
+            periodboard.setText("Έληξε");
+        }else
+        {
+            periodboard.setText(MySQL.getMatchColumn(m.getId(),"period")+"η Περίοδος");
+        }
+    }
+
     private int getCurrentSeconds(){
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HHmmss");
@@ -177,7 +213,7 @@ public class ingame extends AppCompatActivity {
     //REFRESH ACTIVITY
     Handler handler = new Handler();
     Runnable runnable;
-    int delay = 3000;
+    int delay = 1000;
 
     @Override
     protected void onResume() {
@@ -185,6 +221,8 @@ public class ingame extends AppCompatActivity {
             handler.postDelayed(runnable = new Runnable() {
                 public void run() {
                     handler.postDelayed(runnable, delay);
+                    updateTimer();
+                    updatePeriod();
                 }
             }, delay);
             super.onResume();
