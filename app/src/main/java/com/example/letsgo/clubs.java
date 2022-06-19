6669f1b5a489ddd873ed753b7a036dd43362197c
XenofontasS <iis20090@uom.edu.gr>
2022-06-20 01:47:52 +0300

    scrollviews

--- app/src/main/java/com/example/letsgo/clubs.java
+++ app/src/main/java/com/example/letsgo/clubs.java
@@ -8,13 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class clubs extends AppCompatActivity {
 
-    LinearLayout layoutList;
-    String name;
+//    LinearLayout layoutList;
+//    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,36 +28,42 @@ public class clubs extends AppCompatActivity {
             public void onClick(View v) {gotomenufan();}
         });
 
-        layoutList = findViewById(R.id.layoutList);
-        try {
-            ArrayList<String> teams = MySQL.getTeams();
-            for (int i=0;i<teams.size();i++)
-            {
-                addView(teams.get(i));
-            }
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
-    }
-
-    private void addView(String n) {
-        View buttonsView = getLayoutInflater().inflate(R.layout.match_button, null, false);
-
-        Button b = buttonsView.findViewById(R.id.Match_1);
-        b.setText(n);
-
-        b.setOnClickListener(new View.OnClickListener() {
+        TextView tv = findViewById(R.id.title);
+        tv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                name = n;
-                gotoclubstats();
-            }
+            public void onClick(View v) {gotoclubstats();}
         });
 
-        buttonsView.setId(View.generateViewId());
-        layoutList.addView(buttonsView);
+//        layoutList = findViewById(R.id.layoutList);
+//        try {
+//            ArrayList<String> teams = MySQL.getTeams();
+//            for (int i=0;i<teams.size();i++)
+//            {
+//                addView(teams.get(i));
+//            }
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
+
     }
+
+//    private void addView(String n) {
+//        View buttonsView = getLayoutInflater().inflate(R.layout.match_button, null, false);
+//
+//        Button b = buttonsView.findViewById(R.id.Match_1);
+//        b.setText(n);
+//
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                name = n;
+//                gotoclubstats();
+//            }
+//        });
+//
+//        buttonsView.setId(View.generateViewId());
+//        layoutList.addView(buttonsView);
+//    }
 
     private void gotomenufan(){
         Intent intent = new Intent(this, menu_fan.class);
@@ -65,9 +72,9 @@ public class clubs extends AppCompatActivity {
 
     private void gotoclubstats(){
         Intent intent = new Intent(this, club_stats.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("name",name);
-        intent.putExtras(bundle);
+//        Bundle bundle = new Bundle();
+//        bundle.putString("name",name);
+//        intent.putExtras(bundle);
         startActivity(intent);
     }
 }
